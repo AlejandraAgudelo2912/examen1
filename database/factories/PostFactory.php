9beebcbfe0f4021c6d8410ad5ae3e95c75ce4e2d
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2024-11-13 15:50:03 +0100

    Ejercicio 2

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -17,8 +18,12 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => $this->faker->sentence(),
-            'body' => $this->faker->paragraph(),
+            'title' => $title = $this->faker->sentence(),
+            'body' => $body = $this->faker->paragraph(),
+            'summary' => Str::limit($body, 50),
+            'slug' => Str::snake($title),
+            'status' => $this->faker->randomElement(['published', 'draft', 'archived', 'pending']),
+            'reading_time' => rand(1,100),
             'published_at' => random_int(0, 2)
                 ? $this->faker->dateTimeBetween('-1 month', '+1 months')
                 : null,
